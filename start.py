e8a423c70f2ae2761ee52d3be33631fac969b6fd
vigneshperiasamy <darkespyt8577@gmail.com>
2024-09-08 18:48:39 +0530

    Source updated

---
+++ start.py
@@ -0,0 +1,35 @@
+from telethon import TelegramClient, events
+import re
+
+API_ID = '21814194'
+API_HASH = '9535a6cee1e1d8e3a97a245f73b2d52a'
+PHONE_NUMBER = '+918678996799'
+TARGET_CHANNELS = ['@DARKESPYT', '@Source_Leak', '@Source_HUB']
+SOURCE_CHANNELS = ['@backuprrrrrr', '@CAPTAINSRC', '@KINGMODEVIPSRC', '@VIP_SRC_Leakers', '@LEAK_SRC_ALL', '@Tharki_Pushpa', '@SRC_BGMI_GL_KR_VNG', '@SrcEsp', '@PrivateFileTg', '@KNIGHTMODSSRCS', '@NOBITA_SRC', '@VIP_SRC_LEEKAR', '@Yarasa_Src', '@SrcLeakerVip', '@MadSrcLeakers', '@PRIVATE_SRC', '@SrcTeam']
+client = TelegramClient('SRC_FORWARDER', API_ID, API_HASH)
+
+async def main():
+    await client.start(PHONE_NUMBER)
+    print("Client Started")
+    print(f"All Source Channels: {SOURCE_CHANNELS}")
+
+@client.on(events.NewMessage(chats=SOURCE_CHANNELS))
+async def forward_message(event):
+    try:
+        if event.photo and 'SRC' in (event.message.message or ''):
+            caption = re.sub(r"http\S+|https\S+|@\w+", "@DARKESPYT", event.message.message or '')
+            for target_channel in TARGET_CHANNELS:
+                await client.send_file(target_channel, event.message.media, caption=caption)
+        elif 'SRC' in (event.message.message or ''):
+            text = re.sub(r"http\S+|https\S+|@\w+", "@DARKESPYT", event.message.message)
+            for target_channel in TARGET_CHANNELS:
+                await client.send_message(target_channel, text)
+        elif event.document and event.document.mime_type == 'application/zip':
+            caption = re.sub(r"http\S+|https\S+|@\w+", "@DARKESPYT", event.message.message or '')
+            for target_channel in TARGET_CHANNELS:
+                await client.send_file(target_channel, event.message.media, caption=caption)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+client.start()
+client.run_until_disconnected()
