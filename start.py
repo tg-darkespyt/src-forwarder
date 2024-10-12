4d5d9dc0dd4bb7a4b76699a22b73b1985008023e
Vigneshwaran P <143394493+tg-darkespyt@users.noreply.github.com>
2024-10-12 10:09:37 +0530

    Update start.py

--- start.py
+++ start.py
@@ -9,10 +9,11 @@ import shutil
 API_ID = '21814194'
 API_HASH = '9535a6cee1e1d8e3a97a245f73b2d52a'
 PHONE_NUMBER = '+918678996799'
+SESSION_FILE = os.getenv('SESSION_FILE')
 TARGET_CHANNELS = ['@DARKESPYT', '@Source_Leak', '@Source_HUB', '@BADBOY_MAIN']
 SOURCE_CHANNELS = ['@backuprrrrrr', '@Dazai_FreeSrc', '@CAPTAINSRC', '@KINGMODEVIPSRC', '@VIP_SRC_Leakers', '@LEAK_SRC_ALL', '@Tharki_Pushpa', '@SRC_BGMI_GL_KR_VNG', '@SrcEsp', '@PrivateFileTg', '@KNIGHTMODSSRCS', '@NOBITA_SRC', '@VIP_SRC_LEEKAR', '@Yarasa_Src', '@SrcLeakerVip', '@MadSrcLeakers', '@PRIVATE_SRC', '@SrcTeam']
 
-client = TelegramClient('SRC_FORWARDER', API_ID, API_HASH)
+client = TelegramClient(SESSION_FILE, API_ID, API_HASH)
 DB_FILE = 'passwords.db'
 conn = sqlite3.connect(DB_FILE)
 cursor = conn.cursor()
@@ -20,7 +21,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS passwords (post_id INTEGER PRIMARY 
 conn.commit()
 
 async def main():
-    await client.start(PHONE_NUMBER)
+    await client.start(phone=lambda: PHONE_NUMBER)
     print("Client Started")
 
 def save_password(post_id, password):
